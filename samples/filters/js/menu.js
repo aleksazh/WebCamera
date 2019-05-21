df590537788a9d3764f9c5f7f2b44164f2f822fd
Aleksandra Zhuravleva <aleksandra.zhuravleva@intel.com>
2019-05-21 16:46:40 +0300

    Add link to home page

--- samples/filters/js/menu.js
+++ samples/filters/js/menu.js
@@ -11,8 +11,34 @@ var currentPosition = 0;
 var menuWrapperSize = menuWrapperElement[0].offsetWidth;
 var menuSize = itemElement.length * itemSize;
 
+var paddleMargin = 20;
+var menuVisibleSize = menuWrapperSize;
+var menuInvisibleSize = menuSize - menuWrapperSize;
+
 window.addEventListener('resize', function () {
 	menuWrapperSize = menuWrapperElement[0].offsetWidth;
+});
+
+menuElement[0].addEventListener('scroll', function () {
+	menuInvisibleSize = menuSize - menuWrapperSize;
+	var menuPosition = currentPosition;
+	var menuEndOffset = menuInvisibleSize - paddleMargin;
+	if (menuPosition <= paddleMargin) {
+		$(leftPaddle).addClass('hidden');
+		$(rightPaddle).removeClass('hidden');
+		//leftPaddle.classList.add('hidden');
+		//rightPaddle.classList.remove('hidden');
+	} else if (menuPosition < menuEndOffset) {
+		$(leftPaddle).removeClass('hidden');
+		$(rightPaddle).removeClass('hidden');
+		//leftPaddle.classList.remove('hidden');
+		//rightPaddle.classList.remove('hidden');
+	} else if (menuPosition >= menuEndOffset) {
+		$(leftPaddle).removeClass('hidden');
+		$(rightPaddle).addClass('hidden');
+		//leftPaddle.classList.remove('hidden');
+		//rightPaddle.classList.add('hidden');
+	}
 });
 
 rightPaddle[0].addEventListener('click', function () {
