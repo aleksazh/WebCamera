310e28f77c379ef6a905a286180ddc21e9b296ba
Aleksandra Zhuravleva <aleksandra.zhuravleva@intel.com>
2019-05-31 17:05:27 +0300

    Second attempt to use web worker

--- samples/faceDetection/js/genderWorker.js
+++ samples/faceDetection/js/genderWorker.js
@@ -1,11 +1,10 @@
+importScripts('../../filters/js/utils.js')
+
 let utils = new Utils('errorMessage');
 
-let stats = null;
 const FPS = 30;
 let resolution = window.innerWidth < 700 ? 'qvga' : 'vga';
 let video = document.getElementById('videoInput');
-let canvasOutput = document.getElementById('canvasOutput');
-let canvasContext = canvasOutput.getContext('2d');
 
 let streaming = false;
 let src = null;
@@ -15,11 +14,9 @@ let faces = null;
 let frameBGR = null;
 
 let classifier = null;
-let genderNet = null;
 let ageNet = null;
 let gender;
 let age;
-let genderWorker;
 
 const faceDetectionPath = 'haarcascade_frontalface_default.xml';
 const faceDetectionUrl = 'classifiers/haarcascade_frontalface_default.xml';
@@ -32,13 +29,16 @@ const ageProtoUrl = 'classifiers/deploy_age.prototxt';
 const ageModelPath = 'age_net.caffemodel';
 const ageModelUrl = 'classifiers/age_net.caffemodel';
 
+const genderList = ['Male', 'Female'];
 const ageList = [
   '(0 - 2)', '(4 - 6)', '(8 - 12)', '(15 - 20)',
   '(25 - 32)', '(38 - 43)', '(48 - 53)', '(60 - 100)'];
+const MODEL_MEAN_VALUES = [78.4263377603, 87.7689143744, 114.895847746, 0];
+const prototxtWidth = 227;
+const prototxtHeight = 227;
 const color = [0, 255, 255, 255];
 
 function startVideoProcessing() {
-  stats = new Stats();
   src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
   dst = new cv.Mat(video.height, video.width, cv.CV_8UC4);
   frameBGR = new cv.Mat(video.height, video.width, cv.CV_8UC3);
@@ -46,18 +46,8 @@ function startVideoProcessing() {
   faces = new cv.RectVector();
   classifier = new cv.CascadeClassifier();
 
-  stats.showPanel(0);
-  document.body.appendChild(stats.domElement);
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.right = '0px';
-  stats.domElement.style.top = '0px';
   // load pre-trained classifier for face detection
   classifier.load(faceDetectionPath);
-  // create worker for gender detection
-  genderWorker = new Worker("js/genderWorker.js");
-  genderWorker.onmessage = function (e) {
-    gender = e.data;
-  }
   // schedule the first processing
   setTimeout(processVideo, 0);
 }
@@ -73,7 +63,6 @@ function processVideo() {
       classifier.delete();
       return;
     }
-    stats.begin();
     let begin = Date.now();
     // start processing
     cap.read(src);
@@ -84,9 +73,17 @@ function processVideo() {
       1.1, 3); // scaleFactor=1.1, minNeighbors=3
     for (let i = 0; i < faces.size(); ++i) {
       let face = faces.get(i);
-      //genderWorker.postMessage(faceFrame, genderNet);
-      //genderWorker.postMessage([faceFrame, genderNet]);
-
+      let faceRect = new cv.Rect(face.x, face.y, face.width, face.height);
+      let faceFrame = frameBGR.roi(faceRect);
+      let blob = cv.blobFromImage(faceFrame, 1.0, // scalefactor=1.0
+        { width: prototxtWidth, height: prototxtHeight },
+        MODEL_MEAN_VALUES, false, false); // swapRB=false, crop=false
+      // detect gender
+      genderNet.setInput(blob);
+      let genderPreds = genderNet.forward();
+      let gender = genderList[
+        genderPreds.data32F.indexOf(Math.max(...genderPreds.data32F))];
+      self.postMessage(gender);
       // detect age
       /*ageNet.setInput(blob);
       let agePreds = ageNet.forward();
@@ -94,29 +91,14 @@ function processVideo() {
         agePreds.data32F.indexOf(Math.max(...agePreds.data32F))];
       // add label with gender and age to face
       let label = gender + " " + age;*/
-      let label;
-      if (typeof gender == "undefined") {
-        label = "NaN";
-      } else {
-        label = gender;
-      }
-      cv.putText(dst, label, { x: face.x, y: face.y - 10 },
-        cv.FONT_HERSHEY_SIMPLEX,
-        0.6, color, 2, cv.LINE_4); // fontScale=0.6, thickness=2
-      // draw face
-      let point1 = new cv.Point(face.x, face.y);
-      let point2 = new cv.Point(face.x + face.width, face.y + face.height);
-      cv.rectangle(dst, point1, point2, color);
 
-      /*faceFrame.delete();
+      faceFrame.delete();
       genderPreds.delete();
-      agePreds.delete();
-      blob.delete();*/
+      //agePreds.delete();
+      blob.delete();
     }
-    cv.imshow('canvasOutput', dst);
     // schedule the next processing
     let delay = 1000 / FPS - (Date.now() - begin);
-    stats.end();
     setTimeout(processVideo, delay);
   } catch (err) {
     utils.printError(err);
@@ -142,7 +124,6 @@ function onVideoStarted() {
 
 function onVideoStopped() {
   streaming = false;
-  canvasContext.clearRect(0, 0, canvasOutput.width, canvasOutput.height);
 }
 
 document.getElementById('status').innerHTML = 'Loading OpenCV...';
