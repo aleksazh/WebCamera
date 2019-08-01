3cbd636a1ddf1a86705bbc94848b2a48ce569c65
Aleksandra Zhuravleva <aleksandra.zhuravleva@intel.com>
2019-08-01 11:05:13 +0300

    Add canvas

--- samples/cardScanner/js/index.js
+++ samples/cardScanner/js/index.js
@@ -1,9 +1,11 @@
 let utils = new Utils('errorMessage');
 let controls = {};
 let videoConstraint;
+let streaming = false;
 let videoTrack = null;
 let imageCapturer = null;
 let video = document.getElementById('videoInput');
+let canvasOutput = document.getElementById('canvasOutput');
 let videoCapturer = null;
 let src = null;
 
@@ -11,13 +13,42 @@ let rectWidth = 50;
 let rectHeight = 25;
 const rectColor = [0, 255, 0, 255]; // Green
 
+
+function initOpencvObjects() {
+  videoCapturer = new cv.VideoCapture(video);
+  src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+}
+
+function completeStyling() {
+  let cameraBar = document.querySelector('.camera-bar-wrapper');
+  cameraBar.style.width = `${video.videoWidth}px`;
+
+  let mainContent = document.getElementById('mainContent');
+  mainContent.style.width = `${video.videoWidth}px`;
+  mainContent.classList.remove('hidden');
+
+  document.querySelector('.canvas-wrapper').style.height =
+    `${video.videoHeight}px`;
+
+  document.getElementById('takePhotoButton').disabled = false;
+  document.getElementById('facingModeButton').disabled = true;
+}
+
 function processVideo() {
-  videoCapturer.read(src);
-  let pointUpperLeft = new cv.Point(50, 50);
-  let pointBottomRight = new cv.Point(50 + rectWidth, 50 + rectHeight);
-  cv.rectangle(src, pointUpperLeft, pointBottomRight, greenColor);
-  // cv.imshow('videoInput', src); // USE CANVAS
-  requestAnimationFrame(processVideo);
+  try {
+    if (!streaming) {
+      src.delete();
+      return;
+    }
+    videoCapturer.read(src);
+    let pointUpperLeft = new cv.Point(50, 50);
+    let pointBottomRight = new cv.Point(50 + rectWidth, 50 + rectHeight);
+    cv.rectangle(src, pointUpperLeft, pointBottomRight, rectColor, 3);
+    cv.imshow('canvasOutput', src);
+    requestAnimationFrame(processVideo);
+  } catch (err) {
+    utils.printError(err);
+  }
 }
 
 function initUI() {
@@ -43,44 +74,18 @@ function initUI() {
   };
 }
 
-function completeStyling() {
-  let cameraBar = document.querySelector('.camera-bar-wrapper');
-  cameraBar.style.width = `${video.videoWidth}px`;
-
-  let mainContent = document.getElementById('mainContent');
-  mainContent.style.width = `${video.videoWidth}px`;
-  mainContent.classList.remove('hidden');
-
-  document.querySelector('.canvas-wrapper').style.height =
-    `${video.videoHeight}px`;
-
-  document.getElementById('takePhotoButton').disabled = false;
-  document.getElementById('facingModeButton').disabled = true;
-}
-
 function startCamera() {
-  utils.startCamera(videoConstraint, 'videoInput', onCameraStarted);
-}
-
-function onCameraStarted() {
-  videoTrack = video.srcObject.getVideoTracks()[0];
-  imageCapturer = new ImageCapture(videoTrack);
-  completeStyling();
-  videoCapturer = new cv.VideoCapture(video);
-  src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-  requestAnimationFrame(processVideo);
+  utils.startCamera(videoConstraint, 'videoInput', onVideoStarted);
 }
 
 function stopCamera(videoElem) {
-  if (videoElem) {
-    videoElem.pause();
-    videoElem.srcObject = null;
-    videoElem.removeEventListener('canplay', utils.onVideoCanPlay);
+  if (!streaming) {
+    utils.clearError();
+    utils.startCamera(videoConstraint, 'videoInput', onVideoStarted);
+  } else {
+    utils.stopCamera();
+    onVideoStopped();
   }
-  if (videoElem.srcObject) {
-    videoElem.srcObject.getVideoTracks()[0].stop();
-  }
-  src.delete();
 };
 
 utils.loadOpenCv(() => {
